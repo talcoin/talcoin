7de622dcb6e46f0c72618cecd99548508b480a55
talcoin <tjeerd_talman@yahoo.com>
2017-06-08 22:25:53 +0200

    ”changes”

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-FooCoin (FOO)
+TalCoin (TAL)
 ===========
 
-[![Build Status](https://travis-ci.org/RazorLove/foocoin.png?branch=master)](https://travis-ci.org/RazorLove/foocoin)
+[![Build Status](https://travis-ci.org/RazorLove/talcoin.png?branch=master)](https://travis-ci.org/RazorLove/talcoin)
 
 
 Scrypt Hashcash PoW Template
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -45,13 +45,13 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("FooCoin");
-    case mBTC: return QString("milliFooCoin (1 / 1,000)");
-    case uBTC: return QString("microFooCoin (1 / 1,000,000)");
+    case BTC: return QString("TalCoin");
+    case mBTC: return QString("milliTalCoin (1 / 1,000)");
+    case uBTC: return QString("microTalCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
-//a single unit (.00000001) of FooCoin is called a "wander."
+//a single unit (.00000001) of TalCoin is called a "wander."
 qint64 BitcoinUnits::factor(int unit)
 {
     switch(unit)

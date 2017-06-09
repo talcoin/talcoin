b677bd58d106b31ee761b4df72ba4b1995e1f69a
talcoin <tjeerd_talman@yahoo.com>
2017-06-09 15:26:24 +0200

    changes 09-06-2017 / 15:30

--- src/main.cpp
+++ src/main.cpp
@@ -1981,7 +1981,7 @@ bool LoadBlockIndex(bool fAllowNew)
         pchMessageStart[1] = 0xc0;
         pchMessageStart[2] = 0xb8;
         pchMessageStart[3] = 0xdb;
-        hashGenesisBlock = uint256("0x");
+        hashGenesisBlock = uint256("0x631adce79177657fc3283bee0c1a8420d170a5c7cb9dadd7fb311c9c17d83aad");
     }
 
     //
@@ -2021,14 +2021,14 @@ bool LoadBlockIndex(bool fAllowNew)
         if (fTestNet)
         {
             block.nTime    = 1496997297;
-            block.nNonce   = 0;
+            block.nNonce   = 451748;
         }
 
         //// debug print
         printf("%s\n", block.GetHash().ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x"));
+        assert(block.hashMerkleRoot == uint256("0x7d817c7e285f6b294aa586cab27e315f41a7f76401c03c323f9d2742d132c21a"));
 
         // If genesis block hash does not match, then generate new genesis hash.
         if (true && block.GetHash() != hashGenesisBlock)
